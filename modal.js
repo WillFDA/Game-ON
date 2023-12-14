8256882bc233f988ddf4c8b8a84e5ef9ebe7ad48
William <william.deazevedopro@gmail.com>
2023-12-14 21:08:03 +0100

    JS working
    Ducoup Fermeture modal fonctionne et supprime la value des champs.

--- modal.js
+++ modal.js
@@ -11,13 +11,40 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+// new close btn constant
+const closeBtn = document.querySelector(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+// closeModal Event
+closeBtn.addEventListener("click", closeModal);
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+  clearModal();
+}
 
+// Declaring all modals input
 
+const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const birthdateInput = document.getElementById("birthdate");
+const quantityInput = document.getElementById("quantity");
+
+// clearing all modal forms
+function clearModal() {
+  console.log(firstNameInput);
+  firstNameInput.value = "";
+  lastNameInput.value = "";
+  emailInput.value = "";
+  birthdateInput.value = "";
+  quantityInput.value = "";
+}
+
+function validate(event) {
+  event.preventDefault();
+}
